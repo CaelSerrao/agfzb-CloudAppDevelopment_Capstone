ef6d69dcc80b6eb2062a7d73a9bdb6542007d410
CaelSerrao <86877852+CaelSerrao@users.noreply.github.com>
2023-06-28 11:54:55 +0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,22 +3,30 @@ from django.utils.timezone import now
 
 
 # Create your models here.
+TIPOLOGIA_CHOICES = [
+    ("SUv", "SUV"),
+    ("Wagon", "Wagon"),
+]
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    Name = models.CharField(null=False, default="name", )
+    Description = models.CharField(null=False, max_length=2000, default="mame")
+    def __str__(self):
+            return "Name: " + self.name + "," + \
+                "Description: " + self.description
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    make=models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    Name = models.CharField(null=False, default="name", )
+    Dealerid = models.IntegerField(default=0)
+    Type = models.CharField(null=False, choices=TIPOLOGIA_CHOICES)
+    Year = models.DateField(default=now)
+
+    def __str__(self):
+                return "Name: " + self.Name + "," + \
+                    "Description: " + self.Dealerid + \
+                    "Type: " + self.Type
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
