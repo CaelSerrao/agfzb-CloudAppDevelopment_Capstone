f31af7dc869429576289662dc0345657eb9a185a
CaelSerrao <86877852+CaelSerrao@users.noreply.github.com>
2023-07-06 16:38:12 +0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,33 +1,89 @@
 from django.db import models
+
 from django.utils.timezone import now
 
 
-# Create your models here.
 TIPOLOGIA_CHOICES = [
+
     ("SUv", "SUV"),
+
     ("Wagon", "Wagon"),
+
 ]
 
+
 class CarMake(models.Model):
-    Name = models.CharField(null=False,max_length=2000, default="name", )
+
+    Name = models.CharField(null=False, max_length=2000, default="name")
+
     Description = models.CharField(null=False, max_length=2000, default="mame")
+
+
     def __str__(self):
-            return "Name: " + self.name + "," + \
-                "Description: " + self.description
+
+        return "Name: " + self.Name + ", " + "Description: " + self.Description
+
 
 
 class CarModel(models.Model):
-    make=models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    Name = models.CharField(null=False, max_length=2000,default="name", )
+
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+
+    Name = models.CharField(null=False, max_length=2000, default="name")
+
     Dealerid = models.IntegerField(default=0)
-    Type = models.CharField(null=False,max_length=2000, choices=TIPOLOGIA_CHOICES)
+
+    Type = models.CharField(null=False, max_length=2000, choices=TIPOLOGIA_CHOICES)
+
     Year = models.DateField(default=now)
 
-    def __str__(self):
-                return "Name: " + self.Name + "," + \
-                    "Description: " + self.Dealerid + \
-                    "Type: " + self.Type
 
+    def __str__(self):
+
+        return (
+
+            "Name: "
+
+            + self.Name
+
+            + ", "
+
+            + "Description: "
+
+            + str(self.Dealerid)  # Convert Dealerid to string
+
+            + ", "
+
+            + "Type: "
+
+            + self.Type
+
+        )
+
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + str(self.full_name)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
