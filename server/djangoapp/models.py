365611659286b69b74bbf4864d18198a88cfc248
CaelSerrao <86877852+CaelSerrao@users.noreply.github.com>
2023-07-20 14:00:52 +0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -85,6 +85,26 @@ class CarDealer:
     def __str__(self):
         return "Dealer name: " + str(self.full_name)
 
+class DealerReview:
+    def __init__(self, dealership, name, purchase,review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        self.dealership = dealership
+        self.name= name
+        self.purchase= purchase
+        self.review=review
+        self.purchase_date=purchase_date
+        self.car_make=car_make
+        self.car_model=car_model
+        self.car_year=year
+        self.senti=sentiment
+        self.id=id
+
+    def __str__(self):
+        return "Dealer review:" +self.review
+
+
+
+
+
 
 
 
